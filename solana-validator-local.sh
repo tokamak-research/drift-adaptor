93b49619bc03f9a64e4ae90e26615f6a7e69d523
tkkinn <tkkinn@protonmail.com>
2024-03-28 14:56:28 +0800

    Initialize Drift

---
+++ solana-validator-local.sh
@@ -0,0 +1,5 @@
+solana-test-validator -r \
+ --bpf-program metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s "./onchain-program/token-metadata.so" \
+ --bpf-program 7MGkDRY9dhNS6KYRBSzrNadH9uuuPeazJm4TdESAwYsb "./onchain-program/token-faucet.so" \
+ --bpf-program dRiftyHA39MWEi3m9aunc5MzRF1JYuBsbn6VPcn33UH "./onchain-program/drift.so" \
+ --bpf-program E6HjiqKytNxovAKfbudZcV72iryoFzP6nL71zM6A4sp5 "./onchain-program/mock-pyth.so" \
